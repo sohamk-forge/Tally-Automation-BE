62486196851d2dcd27cefb253a12f80c12eb246e
SK-4 <sohamk2002@gmail.com>
2026-08-06 15:25:38 +0530

    Revert "fix app test v1 : new knex migrations v2 table"
    This reverts commit f0fbff8934b0c261030450c291a7a8f1fdd49983.

--- migrations_v2/20260805150057_add_user_id_to_contra_vouchers.js
+++ migrations_v2/20260805150057_add_user_id_to_contra_vouchers.js
@@ -1,9 +1,7 @@
 // migrations/xxxxxx_add_user_id_to_contra_vouchers.js
-import { DB_SCHEMA } from "../src/config/db.js";
-
 export async function up(knex) {
   await knex.schema
-    .withSchema(DB_SCHEMA)
+    .withSchema("app_test")
     .alterTable("contra_vouchers", (table) => {
       table.integer("user_id");
     });
@@ -11,7 +9,7 @@ export async function up(knex) {
 
 export async function down(knex) {
   await knex.schema
-    .withSchema(DB_SCHEMA)
+    .withSchema("app_test")
     .alterTable("contra_vouchers", (table) => {
       table.dropColumn("user_id");
     });
